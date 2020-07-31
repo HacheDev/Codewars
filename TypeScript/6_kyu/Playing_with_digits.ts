c57b6d70985f90a3d9ac73bae9f31ea0db5308ec
Hache <javierhache16@gmail.com>
2020-07-31 19:22:53 +0200

    kata playing wth digits works now

--- TypeScript/6_kyu/Playing_with_digits.ts
+++ TypeScript/6_kyu/Playing_with_digits.ts
@@ -5,8 +5,7 @@ export class G964 {
       let res: number = 0;
       let _i: number;//contador _i
       for(_i = 0; _i < num.length; ++_i)  {  //iterate n, do  (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) 
-        let aux: number = +num[_i];
-        res += aux**p;
+        res += (+num[_i])**p;
         ++p;
       }
       return Number.isInteger(res/n) ? res/n : -1;
