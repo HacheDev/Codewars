da02c71d083507157c4a53083f1773c98ab300fa
HacheDev <hache.dev16@gmail.com>
2023-01-14 20:39:25 +0100

    Refactoring folder structure and adding duplicate encoder solution for typescript

--- C++/8_kyu/even_or_odd/solution.cpp
+++ C++/8_kyu/even_or_odd/solution.cpp
@@ -1,6 +1,4 @@
 std::string even_or_odd(int number) 
 {
-  
   return number % 2 == 0 ? "Even" : "Odd";
-  
 }
--- Java/8_kyu/multiply/solution.java
+++ Java/8_kyu/multiply/solution.java
@@ -1,4 +1,5 @@
-public class Multiply {
+package multiply;
+public class solution {
     public static Double multiply(Double a, Double b) {
         return a * b;
     }
